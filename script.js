0c29eee4fa370fd7ce3e969a275ddc06433eeeeb
Jaret Cleveland <81397585+jaretcleveland@users.noreply.github.com>
2025-11-01 17:42:22 -0500

    Implement arithmetic operations in the calculator.

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-// javascript
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a ,b) {
+	return a / b;
+};
+
+const operate = function(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return null;
+  };
+};
+
+const display = document.querySelector('#display');
+const digitButtons = document.querySelectorAll('[data-action="digit"]');
+
+let currentDisplay = '1234'; // Default display value for testing
+
+display.textContent = currentDisplay; // Update the display with the current value
+
+// Add event listeners to digit buttons
+digitButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const digit = button.getAttribute("data-value");
+    currentDisplay += digit;
+    display.textContent = currentDisplay;
+  });
+});
